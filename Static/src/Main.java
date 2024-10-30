a0fa58c7975d3070a12c27d036aec1cce2992e19
John Dakin <jdakin749@gmail.com>
2024-10-30 12:01:00 +0300

    Add files via upload

---
+++ Static/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Friend friend1 = new Friend("Spongebob");
+		Friend friend2 = new Friend("Patrick");
+		Friend friend3 = new Friend("Squidward");
+		Friend friend4 = new Friend("Sandy");
+		
+		Friend.displayFriends();
+
+	}
+
+}
